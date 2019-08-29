9701873f4997c4a7a26f9afe1fa3534efa26e500
unmateo <mharfuch@gmail.com>
2019-08-29 19:53:47 -0300

    agrego optimizacion en lectura de dataframe

--- utils/dataset_parsing.py
+++ utils/dataset_parsing.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[84]:
 
 
 import pandas as pd
@@ -9,20 +9,36 @@ import pandas as pd
 FULL_DATASET = "../enunciado/data/train.csv"
 
 
-# In[2]:
+# In[99]:
 
 
-def levantar_datos(csv_file: str = FULL_DATASET) -> pd.DataFrame:
+def levantar_datos(csv_file: str = FULL_DATASET, optimizar: bool = True) -> pd.DataFrame:
     """
         Recibe un .csv que debe tener el formato de columnas específico del tp.
-        Devuelve un Dataframe optimizado con esa información.
+        Si optimizar = True, asigna mejores tipos y agrega columnas útiles.
+        Devuelve un Dataframe con esa información.
     """
-    df = pd.read_csv(csv_file)
+    if not optimizar:
+        return pd.read_csv(csv_file)
+    dtypes = {
+        "piscina": "bool",
+        "escuelascercanas": "bool",
+        "centroscomercialescercanos": "bool",
+        "usosmultiples": "bool",
+        "gimnasio": "bool",
+        "garages": "float16",
+        "banos": "float16",
+        "tipodepropiedad": "category",
+        "ciudad": "category",
+        "provincia": "category",
+        "antiguedad": "float16",
+        "habitaciones": "float16",
+        "metroscubiertos": "float32",
+        "metrostotales": "float32",
+    }
+    df = pd.read_csv(csv_file, dtype=dtypes, parse_dates=["fecha"])
+    df["mes"] = df["fecha"].dt.month
+    df["ano"] = df["fecha"].dt.year
+    df["dia"] = df["fecha"].dt.day
     return df
-
-
-# In[7]:
-
-
-df.columns
 
