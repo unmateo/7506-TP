62945e3a45fb8cd74e649153fc7ffc66c05e5ac6
unmateo <mharfuch@gmail.com>
2019-09-05 19:34:23 -0300

    agrego notebooks por tema

---
+++ src/tp1/lab/caracteristicas_barrio.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Campos involucrados
+# 
+# - escuelascercanas
+# - centroscomercialescercanos
+# - idzona
+# 
+# ## Ideas
+# 
+# - Agregar info externa
---
+++ src/tp1/lab/temporal.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Ideas
+# 
+# - Cantidad de publicaciones de cada tipo a lo largo del tiempo
+# - 
