492991943d0d4be71c0ee76ea2e20f73304b4870
unmateo <mharfuch@gmail.com>
2019-09-08 20:49:41 -0300

    empiezo analisis temporal y de palabras agrego nltk al pipfile para palabras

--- src/tp1/lab/temporal.py
+++ src/tp1/lab/temporal.py
@@ -3,5 +3,99 @@
 
 # ## Ideas
 # 
-# - Cantidad de publicaciones de cada tipo a lo largo del tiempo
+# - Cantidad de publicaciones de cada tipo
+#   - En total
+#   - Por año
 # - 
+
+# In[1]:
+
+
+#importo las funciones para levantar los dataframes
+get_ipython().run_line_magic('run', '"../../utils/dataset_parsing.ipynb"')
+#importo las funciones para graficar
+get_ipython().run_line_magic('run', '"../../utils/graphs.ipynb"')
+df = levantar_datos("../../"+DATASET_RELATIVE_PATH)
+df.columns
+
+
+# In[67]:
+
+
+por_tipo = df.groupby(["tipodepropiedad"]).agg({"tipodepropiedad":"count"}).unstack(fill_value=0).sort_values(ascending=False)
+por_tipo.index = por_tipo.index.droplevel()
+total_publicaciones = por_tipo.sum()
+por_tipo_porcentual = por_tipo.apply(lambda x: round(100 * x/total_publicaciones, 2))
+por_tipo_porcentual
+
+
+# In[58]:
+
+
+por_tipo_porcentual.head(5).sum()
+
+
+# En base a los resultados de la tabla anterior, nos quedamos con los 5 tipos de propiedad más frecuentes en las publicaciones (96.41% del total) para poder sintetizar mejor los gráficos.
+
+# In[76]:
+
+
+top_5_tipos  = por_tipo_porcentual.head(5).index.values
+
+
+# In[85]:
+
+
+df_top_5 = df.loc[df["tipodepropiedad"].isin(top_5_tipos)]
+
+
+# In[87]:
+
+
+por_tipo_ano = df_top_5.groupby(["ano","tipodepropiedad"]).agg({"tipodepropiedad":"count"}).unstack(fill_value=0)
+por_tipo_ano.columns = por_tipo_ano.columns.droplevel()
+
+
+# In[89]:
+
+
+totales = por_tipo_ano.sum(axis=1)
+
+
+# In[90]:
+
+
+# por_tipo_porcentual = por_tipo.apply(lambda x: x[totales], axis=1)
+por_tipo_ano_porcentual = por_tipo_ano.apply(lambda x: round(100 * x/totales[x.index], 2), axis=0)
+
+
+# In[92]:
+
+
+por_tipo_ano_porcentual
+
+
+# In[105]:
+
+
+por_tipo_ano_porcentual.plot(kind='line', xticks=por_tipo_ano_porcentual.index.values)
+
+
+# In[197]:
+
+
+import matplotlib.pyplot as plt
+def get_lineplot(data, xlabel="", ylabel="", **kwargs):
+    plot = data.plot(figsize=(12,6), fontsize=12, **kwargs)
+    plt.legend(bbox_to_anchor=(1.04,1), loc="upper left")
+    plt.xlabel(xlabel, fontsize=14)
+    plt.ylabel(ylabel, fontsize=14)
+    return plot
+plot = get_lineplot(por_tipo_ano_porcentual, xticks=por_tipo_ano_porcentual.index.values, title = "Porcentaje de publicaciones por tipo", xlabel="Año", ylabel="Porcentaje")
+
+
+# In[ ]:
+
+
+
+
--- src/tp1/lab/titulo_descripcion.py
+++ src/tp1/lab/titulo_descripcion.py
@@ -13,3 +13,107 @@
 # - stemming
 # 
 # 
+
+# In[3]:
+
+
+#importo las funciones para levantar los dataframes
+get_ipython().run_line_magic('run', '"../../utils/dataset_parsing.ipynb"')
+#importo las funciones para graficar
+get_ipython().run_line_magic('run', '"../../utils/graphs.ipynb"')
+df = levantar_datos("../../"+DATASET_RELATIVE_PATH)
+df.columns
+
+
+# In[4]:
+
+
+import nltk  
+from nltk.corpus import stopwords  
+from string import punctuation  
+
+
+# In[5]:
+
+
+spanish_stopwords = set(stopwords.words('spanish'))
+non_words = set(punctuation)
+non_words.update({'¿', '¡'})
+non_words.update(map(str,range(10)))
+
+
+# In[79]:
+
+
+def add_to_dict(word, dictionary):
+    dictionary[word] = dictionary.get(word, 0) + 1 
+
+def get_word_count(series):
+    """
+    
+    """
+    words_count = {}
+    no_words_count = {}
+    for title in series.values:
+        if not isinstance(title,str): continue
+        words = title.split()
+        for word in words:
+            text = ''.join([" " if c in non_words else c for c in word]).strip()
+            if text in spanish_stopwords: 
+                add_to_dict(text, no_words_count)
+                continue
+            add_to_dict(text, words_count)
+    return words_count, no_words_count
+
+
+# In[116]:
+
+
+from collections import Counter
+
+def get_word_counters(series):
+    """
+        Faltaría hacer stemming
+    """
+    words_counter = Counter()
+    no_words_counter = Counter()
+    for title in series.values:
+        if not isinstance(title,str): continue
+        words = filter(lambda x: len(x)>2, title.split())
+        for word in words:
+            text = ''.join([" " if c in non_words else c for c in word]).strip()
+            text_ok = len(text) > 2 and not text in spanish_stopwords
+            counter_to_update = words_counter if text_ok else no_words_counter 
+            counter_to_update.update({text})
+    return words_counter, no_words_counter
+
+
+# In[117]:
+
+
+titulo_palabras, titulo_no_palabras = get_word_counters(df["titulo"])
+
+
+# In[119]:
+
+
+descripcion_palabras, descripcion_no_palabras = get_word_counters(df["descripcion"])
+
+
+# In[120]:
+
+
+print(len(titulo_palabras), len(titulo_no_palabras), len(descripcion_palabras), len(descripcion_no_palabras))
+
+
+# In[118]:
+
+
+titulo_palabras.most_common(20)
+
+
+# In[122]:
+
+
+descripcion_palabras.most_common(20)
+
