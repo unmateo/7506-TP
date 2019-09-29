222c5c8e4bea1ab8307877fd76c60f1e5d5ebee7
unmateo <mharfuch@gmail.com>
2019-09-29 10:23:17 -0300

    cierro titulo_descripcion

--- src/tp1/lab/titulo_descripcion.py
+++ src/tp1/lab/titulo_descripcion.py
@@ -38,6 +38,8 @@ get_ipython().run_line_magic('run', '"../../utils/graphs.ipynb"')
 df = levantar_datos("../../"+DATASET_RELATIVE_PATH)
 df.columns
 pd.set_option("display.max_colwidth", -1)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
 
 
 # In[2]:
@@ -105,6 +107,62 @@ df["len_descripcion"] = df["descripcion_limpia"].map(lambda x: len(x.split()))
 
 df["titulo_limpio"] = df["titulo"].map(limpiar_campo)
 df["len_titulo"] = df["titulo_limpio"].map(lambda x: len(x.split()))
+
+
+# In[54]:
+
+
+df.columns
+
+
+# In[135]:
+
+
+# Busco la correlacion entre longitud de descripción y precio
+df[["len_descripcion"," "]].corr()
+
+
+# In[53]:
+
+
+# Para comparar, busco la correlación entre metrostotales y precio
+df[["metrostotales","precio"]].corr()
+
+
+# In[137]:
+
+
+df.corr()["precio_metro_total"]
+
+
+# In[138]:
+
+
+df.corr()["precio_metro_cubierto"]
+
+
+# In[61]:
+
+
+plot = get_barplot(df["len_descripcion"].value_counts().sort_index(), title="Palabras en descripcion", x_label="Cantidad de palabras en descripcion", y_label="Cantidad de publicaciones")
+plot.figure.savefig("../graficos/barplot_palabras_descripcion.png")
+
+
+# In[105]:
+
+
+serie = df["len_descripcion"]
+
+
+# In[134]:
+
+
+plot = serie.plot(kind="hist",figsize=(24,12),xticks=range(0,500,20), bins=25, logy=True, cmap="summer")
+fontdict={"fontsize":18}
+plot.set_title("Palabras en descripcion (log)", fontdict=fontdict)
+plot.set_xlabel("Cantidad de palabras en descripcion", fontdict=fontdict)
+plot.set_ylabel("Cantidad de publicaciones", fontdict=fontdict)
+plot.figure.savefig("../graficos/hist_palabras_descripcion")
 
 
 # In[ ]:
@@ -191,18 +249,20 @@ df["cantidad_palabras_negativas_descripcion"] = df["palabras_negativas_descripci
 df[["cantidad_palabras_negativas_descripcion","precio"]].corr()
 
 
-# In[43]:
+# In[50]:
 
 
 counter_positivas = get_word_counter(df["palabras_positivas_descripcion"])
 wc_positivas = get_wordcloud(counter_positivas)
+wc_positivas.to_file("../graficos/wordcloud_positivas.png")
 
 
-# In[44]:
+# In[49]:
 
 
 counter_negativas = get_word_counter(df["palabras_negativas_descripcion"])
-wc_ngativas = get_wordcloud(counter_negativas)
+wc_negativas = get_wordcloud(counter_negativas)
+wc_negativas.to_file("../graficos/wordcloud_negativas.png")
 
 
 # In[ ]:
@@ -248,12 +308,6 @@ for palabra in palabras_positivas:
 # In[ ]:
 
 
-
-
-
-# In[ ]:
-
-
 top = list(set(test.corr()["metrostotales"].sort_values(ascending=False).head(8).index).union(set(test.corr()["precio"].sort_values(ascending=False).head(8).index)))
 
 
@@ -270,71 +324,4 @@ test_corr["dif"] = abs(test_corr["dif"])
 
 test_corr["dif"].sort_values(ascending=False)
 #estas se me ocurre que serian las palabras que mayor diferencia podrian hacer
-
-
-# In[ ]:
-
-
-test_corr
-
-
-# In[ ]:
-
-
-con_descripcion_y_titulo = df.loc[(df["len_descripcion"]>0) & (df["len_titulo"]>0)]
-
-
-# In[ ]:
-
-
-con_descripcion_y_titulo["titulo_descripcion"] = con_descripcion_y_titulo["titulo_limpio"] + "_" + con_descripcion_y_titulo["descripcion_limpia"]
-con_descripcion_y_titulo["tiene_duplicado"] = con_descripcion_y_titulo["titulo_descripcion"].duplicated(keep=False)
-
-
-# In[ ]:
-
-
-duplicadas = con_descripcion_y_titulo.loc[con_descripcion_y_titulo["tiene_duplicado"]].sort_values(by="titulo_descripcion")
-
-
-# In[ ]:
-
-
-grouped = duplicadas.groupby(["titulo_descripcion"]).agg({"fecha":"nunique", "precio": ["nunique", "mean", "max", "min"]})
-
-
-# In[ ]:
-
-
-grouped.columns = [x+"_"+y for x,y in grouped.columns]
-
-
-# In[ ]:
-
-
-grouped["precio_dif"] = (grouped["precio_max"] - grouped["precio_min"]).astype(int)
-
-
-# In[ ]:
-
-
-grouped["precio_dif"].describe()
-
-
-# In[ ]:
-
-
-grouped.loc[grouped["precio_dif"]>grouped["precio_dif"].mean()].sort_values(by="precio_nunique", ascending=False)
-
-
-# In[ ]:
-
-
-con_descripcion_y_titulo.loc[con_descripcion_y_titulo["titulo_descripcion"]=="venta casa tijuana_lote manzana nocnok encuentra vivienda"].head(2)
-
-
-# In[ ]:
-
-
-con_descripcion_y_titulo.loc[con_descripcion_y_titulo["titulo_descripcion"]=="venta casa chihuahua_nocnok tipo credito contado venta posesion fisica ningun acepta exclusiva"].head(2)
 
